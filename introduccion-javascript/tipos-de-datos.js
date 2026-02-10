f5287d427e130ce88a5f2ab94274bcede17c826e
elisepro <zmremo04@gmail.com>
2026-02-10 10:16:28 -0600

    Reubicacion de archivo

---
+++ introduccion-javascript/tipos-de-datos.js
@@ -0,0 +1,22 @@
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -111);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof '');
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+// Ejemplos adicionales
+
+// FALSE en mayúsculas NO es booleano en JavaScript
+console.log(typeof FALSE);
+
+console.log(typeof 3.14);
+console.log(typeof "Hola mundo");
+console.log(typeof false);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function(){});
+
